cfa7af60492337006c2c51dc9116986d0b8e10be
Davide Ceresa <ceredavide@live.it>
2024-05-12 17:20:16 +0200

    feat: converted to compose structure

--- app/src/main/java/ch/hslu/mobpro/diabetes/MainActivity.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/MainActivity.kt
@@ -1,32 +1,54 @@
 package ch.hslu.mobpro.diabetes
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import ch.hslu.mobpro.diabetes.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
-class MainActivity : AppCompatActivity() {
+import ch.hslu.mobpro.diabetes.ui.navigation.BottomNavigationBar
+import ch.hslu.mobpro.diabetes.ui.screens.HomeScreen
+import ch.hslu.mobpro.diabetes.ui.screens.ProductsScreen
+import ch.hslu.mobpro.diabetes.ui.screens.ProfileScreen
+import ch.hslu.mobpro.diabetes.ui.screens.WelcomeScreen
 
-private lateinit var binding: ActivityMainBinding
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-     binding = ActivityMainBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+        val sharedPref = this.getSharedPreferences("UserInfo", Context.MODE_PRIVATE)
+        val isFirstTime = sharedPref.getBoolean("isFirstTime", false)
 
-        val navView: BottomNavigationView = binding.navView
+        setContent {
+            if (isFirstTime) {
+                WelcomeScreen(onCompleted = {
+                    sharedPref.edit().putBoolean("isFirstTime", false).apply()
+                    setContent { App() }
+                })
+            } else {
+                App()
+            }
+        }
+    }
+}
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun App() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavigationBar(navController) }
+    ) {
+        NavHost(navController, startDestination = "home") {
+            composable("home") { HomeScreen() }
+            composable("dashboard") { ProductsScreen() }
+            composable("notifications") { ProfileScreen() }
+        }
     }
 }

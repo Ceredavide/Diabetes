63e53897008a30cb67736cef9266bbd42d69ffb9
Davide Ceresa <ceredavide@live.it>
2024-05-13 13:53:26 +0200

    feat: add user info saving functionality

--- app/src/main/java/ch/hslu/mobpro/diabetes/data/pref/PreferenceManager.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/data/pref/PreferenceManager.kt
@@ -2,6 +2,7 @@ package ch.hslu.mobpro.diabetes.data.pref
 
 import android.content.Context
 import android.content.SharedPreferences
+import ch.hslu.mobpro.diabetes.ui.screens.welcome.UserPreferences
 
 class PreferenceManager(context: Context) {
     private val sharedPreferences: SharedPreferences = context.getSharedPreferences("UserInfo", Context.MODE_PRIVATE)
@@ -12,6 +13,12 @@ class PreferenceManager(context: Context) {
 
     fun setFirstTime(isFirstTime: Boolean) {
         sharedPreferences.edit().putBoolean("isFirstTime", isFirstTime).apply()
+    }
+
+    fun setUserinfo(userInfo: UserPreferences) {
+        sharedPreferences.edit().putString("name", userInfo.name.value).apply()
+        sharedPreferences.edit().putString("age", userInfo.age.value).apply()
+        sharedPreferences.edit().putString("email", userInfo.email.value).apply()
     }
 
 }

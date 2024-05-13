409cb5c77a00de1915cff6f01fa8b28892c946dd
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-05-13 14:36:19 +0200

    Created ViewModel and Fragment for entering products manually

---
+++ app/src/main/java/ch/hslu/mobpro/diabetes/ui/manualenter/EnterManualFragment.kt
@@ -0,0 +1,40 @@
+package ch.hslu.mobpro.diabetes.ui.manualenter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ch.hslu.mobpro.diabetes.databinding.FragmentDashboardBinding
+import ch.hslu.mobpro.diabetes.ui.dashboard.DashboardViewModel
+
+class EnterManualFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val enterManualViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        enterManualViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

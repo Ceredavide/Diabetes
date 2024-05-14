dd539b8cfe2aa6c8539e8e9c06f7980461293674
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-05-14 22:26:23 +0200

    refactor: Added TextInput

--- app/src/main/java/ch/hslu/mobpro/diabetes/ui/screens/EnterManual.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/ui/screens/EnterManual.kt
@@ -1,13 +1,48 @@
 package ch.hslu.mobpro.diabetes.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun EnterManualScreen() {
-    // Observing the LiveData from the ViewModel
-    val text = "This is the Home Screen"
 
-    // Displaying the text in a Text composable
-    Text(text = text)
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Top
+    ) {
+
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("PRODUCT NAME") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Entered text: ${text.text}",
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+    }
 }

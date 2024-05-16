08748a4f79c66ee91e2098d99494d3336245f9d2
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-05-16 22:29:33 +0200

    moved route string to Route object for easy access

--- app/src/main/java/ch/hslu/mobpro/diabetes/ui/navigation/Routes.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/ui/navigation/Routes.kt
@@ -1,4 +1,11 @@
 package ch.hslu.mobpro.diabetes.ui.navigation
 
 object Routes {
+
+    val home = "home"
+    val dashboard = "dashboard"
+    val notifications = "dashboard"
+    val enterManually = "enter_manually"
+    val editProduct = "edit_product"
+    val searchLocal = "search_local"
 }

8f2a95c9eac76491b91872805e2ae78eb6ed2e37
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-05-17 02:28:25 +0200

    Made SeachLocal update when deleting a Product

--- app/src/main/java/ch/hslu/mobpro/diabetes/ui/components/ProductListItem.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/ui/components/ProductListItem.kt
@@ -1,27 +1,15 @@
 package ch.hslu.mobpro.diabetes.ui.components
 
-import android.os.Bundle
-import android.provider.Settings.Global.putString
-import android.util.Log
-import android.widget.Button
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -29,33 +17,26 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import ch.hslu.mobpro.diabetes.MainActivity
 import ch.hslu.mobpro.diabetes.database.Product
 import ch.hslu.mobpro.diabetes.ui.navigation.Routes
-import ch.hslu.mobpro.diabetes.ui.screens.EditProduct
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 @Composable
-fun ProductListItem(navController: NavController, product: Product, editable: Boolean) {
+fun ProductListItem(navController: NavController,
+                    product: Product,
+                    editable: Boolean) {
 
     Row (
         modifier = Modifier
@@ -87,7 +68,9 @@ fun ProductListItem(navController: NavController, product: Product, editable: Bo
 }
 
 @Composable
-private fun EditDeleteIcons(navController: NavController, product: Product,  modifier: Modifier = Modifier) {
+private fun EditDeleteIcons(navController: NavController,
+                            product: Product,
+                            modifier: Modifier = Modifier) {
 
     val context = LocalContext.current
 
@@ -118,6 +101,7 @@ private fun EditDeleteIcons(navController: NavController, product: Product,  mod
                         MainActivity.productDao.deleteProductByName(product.name!!)
                     }
                     Toast.makeText(context, "DELETED PRODUCT ${product.name}", Toast.LENGTH_LONG).show()
+                    navController.navigate(Routes.searchLocal)
                 }
         )
     }

651641c208bd30a4ddcacb2e10a235244f778733
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-05-17 02:34:25 +0200

    refactor: Removed unneccesary impot

--- app/src/main/java/ch/hslu/mobpro/diabetes/ui/screens/EnterManual.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/ui/screens/EnterManual.kt
@@ -1,6 +1,5 @@
 package ch.hslu.mobpro.diabetes.ui.screens
 
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column

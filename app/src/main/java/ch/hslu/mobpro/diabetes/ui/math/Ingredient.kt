c6e487d24f2704ce4a22bd99639f3454a80a147b
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-05-18 15:57:05 +0200

    refactor: wieghtAmount is now nullable

--- app/src/main/java/ch/hslu/mobpro/diabetes/ui/math/Ingredient.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/ui/math/Ingredient.kt
@@ -1,11 +1,12 @@
 package ch.hslu.mobpro.diabetes.ui.math
 
+import androidx.compose.ui.geometry.times
 import ch.hslu.mobpro.diabetes.database.Product
 
-class Ingredient(val product: Product, var weightAmount: Float) {
+class Ingredient(val product: Product, var weightAmount: Float?) {
 
     fun calculateCarbs(): Float {
 
-        return (product.carbs?.div(100.0f) ?: 1.0f) * weightAmount
+        return (product.carbs?.div(100.0f) ?: 1.0f) * weightAmount!!
     }
 }

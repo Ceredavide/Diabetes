bf2867ee4a2191c4035832dfe7f0b45bd28f4082
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-05-18 16:03:12 +0200

    refactor: removed unnecessary functions

--- app/src/main/java/ch/hslu/mobpro/diabetes/database/ProductDAO.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/database/ProductDAO.kt
@@ -12,11 +12,11 @@ interface ProductDAO {
 
     @Query("SELECT * FROM product")
     fun getAll(): List<Product>
-    @Query("SELECT * FROM product WHERE uid IN (:productIds)")
-    fun loadAllById(productIds: IntArray): List<Product>
+    //@Query("SELECT * FROM product WHERE uid IN (:productIds)")
+    //fun loadAllById(productIds: IntArray): List<Product>
 
-    @Query("SELECT * FROM product WHERE uid IN (:names)")
-    fun loadAllByName(names: List<String>): List<Product>
+    //@Query("SELECT * FROM product WHERE uid IN (:names)")
+    //fun loadAllByName(names: List<String>): List<Product>
 
     @Query("SELECT * FROM product WHERE product_name = :name")
     fun getProductByName(name: String): Product?

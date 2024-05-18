26b394c21408f648b30ddea0c90aef04350f26f3
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-05-18 16:00:22 +0200

    refactor: uses operators instead of functions

--- app/src/main/java/ch/hslu/mobpro/diabetes/ui/viewmodels/IngredientViewModel.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/ui/viewmodels/IngredientViewModel.kt
@@ -1,22 +1,23 @@
 package ch.hslu.mobpro.diabetes.ui.viewmodels
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ch.hslu.mobpro.diabetes.ui.math.Ingredient
 
 class IngredientViewModel: ViewModel() {
 
     var ingredients = mutableStateListOf<Ingredient>()
-
     fun addIngredient(ingredient: Ingredient) {
 
-        ingredients.add(ingredient)
+        ingredients += ingredient
     }
 
     fun removeIngredient(ingredient: Ingredient) {
 
-        ingredients.remove(ingredient)
+        ingredients -= ingredient
     }
     fun clearIngredients() {
 

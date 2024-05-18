db1d31b30e8b9892a99268c5083b086fc737c02d
Davide Ceresa <ceredavide@live.it>
2024-05-19 00:04:57 +0200

    chore: fixed dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 
     //RoomDB
-    //id("com.google.devtools.ksp")//version"1.9.22-1.0.17"apply false
+    //id("com.google.devtools.ksp") version"1.9.22-1.0.17"apply false
 }

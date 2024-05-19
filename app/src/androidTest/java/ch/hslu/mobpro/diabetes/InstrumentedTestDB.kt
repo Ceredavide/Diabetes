7cb168e7f07673d48e21caa972bd5f6233ac6e2c
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-05-20 00:05:51 +0200

    refactor: fixed imports

--- app/src/androidTest/java/ch/hslu/mobpro/diabetes/InstrumentedTestDB.kt
+++ app/src/androidTest/java/ch/hslu/mobpro/diabetes/InstrumentedTestDB.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import ch.hslu.mobpro.diabetes.database.AppDatabase
-import ch.hslu.mobpro.diabetes.database.Product
-import ch.hslu.mobpro.diabetes.database.ProductDAO
+import ch.hslu.mobpro.diabetes.data.database.AppDatabase
+import ch.hslu.mobpro.diabetes.data.database.Product
+import ch.hslu.mobpro.diabetes.data.database.ProductDAO
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test

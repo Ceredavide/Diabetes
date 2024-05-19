4fc6b471fe33f660c6e59eae8825ca4e2c189171
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-05-19 16:40:30 +0200

    refactor: removed unnecessary import

--- app/src/main/java/ch/hslu/mobpro/diabetes/ui/components/ProductListItem.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/ui/components/ProductListItem.kt
@@ -29,7 +29,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import ch.hslu.mobpro.diabetes.MainActivity

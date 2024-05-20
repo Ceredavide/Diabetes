4d43b4a0fc1eaeaad59067a3a65bac8019c8d3b9
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-05-20 21:20:13 +0200

    Implemented reusable composable for edit and delete button pair

---
+++ app/src/main/java/ch/hslu/mobpro/diabetes/ui/components/EditDeleteButtons.kt
@@ -0,0 +1,67 @@
+package ch.hslu.mobpro.diabetes.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun EditDeleteButtons(modifier: Modifier,
+                      onEdit: () -> Unit,
+                      onDelete: () -> Unit,
+                      deletable: Boolean) {
+
+    Row(modifier = modifier) {
+
+        IconButton(
+            modifier = Modifier
+                .clip(RoundedCornerShape(4.dp))
+                .background(Color.LightGray)
+                .padding(4.dp),
+            onClick = onEdit
+        ) {
+            Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit")
+        }
+
+        Spacer(modifier = Modifier.padding(3.dp))
+
+        if (deletable) {
+
+            IconButton(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(4.dp))
+                    .background(Color.Red)
+                    .padding(4.dp),
+                onClick = onDelete
+            ) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
+            }
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun EditDeleteButtonsPreview() {
+
+    val modifier = Modifier.padding(16.dp)
+    val onEdit =  {}
+    val onDelete =  {}
+
+    EditDeleteButtons(modifier, onEdit, onDelete, true)
+}

76a6ddb7c2cc0b674a83b12de39a398f91b3727f
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-05-20 21:23:22 +0200

    Added routes

--- app/src/main/java/ch/hslu/mobpro/diabetes/ui/navigation/Routes.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/ui/navigation/Routes.kt
@@ -10,4 +10,6 @@ object Routes {
     val searchLocal = "search_local"
     val composeMeal = "compose_meal"
     val resultScreen = "result_screen"
+    val editUser = "edit_user"
+    val addUser = "add_user"
 }

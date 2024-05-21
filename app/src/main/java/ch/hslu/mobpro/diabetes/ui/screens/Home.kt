4deac36c865d6f313a0e21e3350178ece877fe1d
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-05-21 22:10:44 +0200

    refactor: Now reading real user data

--- app/src/main/java/ch/hslu/mobpro/diabetes/ui/screens/Home.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/ui/screens/Home.kt
@@ -4,17 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.material.Text
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,18 +18,18 @@ import ch.hslu.mobpro.diabetes.data.database.GlucoseReading
 import ch.hslu.mobpro.diabetes.data.pref.PreferenceManager
 import ch.hslu.mobpro.diabetes.ui.components.ActiveUserIndicator
 import ch.hslu.mobpro.diabetes.ui.components.Graph
-import co.yml.charts.common.model.Point
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.Date
 import kotlin.random.Random
 
 @Composable
 fun HomeScreen() {
 
-    var readings by remember { mutableStateOf<List<GlucoseReading>>(emptyList()) }
-    val points = remember { mutableStateOf<List<Point>>(emptyList()) }
+    val readings = remember { mutableStateOf<List<GlucoseReading>>(emptyList()) }
+    getGlucoseReadings(readings)
 
     Column(
             verticalArrangement = Arrangement.Top
@@ -42,10 +37,7 @@ fun HomeScreen() {
 
         ActiveUserIndicator()
 
-        //val points = listOf( Point(0.0f, 40.0f), Point(1.0f, 90.0f), Point(2.0f, 0.0f), Point(3.0f, 60.0f), Point(4.0f, 10.0f) )
-
         Graph(
-                //points = points.value,
                 readings = readings,
                 modifier = Modifier
                     .fillMaxWidth()
@@ -55,7 +47,6 @@ fun HomeScreen() {
         Button(
                 modifier = Modifier,
                 onClick = {
-                    readings = generateData()
                 }
         ) {
             Text("Generate Data")
@@ -63,16 +54,33 @@ fun HomeScreen() {
     }
 }
 
-private fun generateData(): List<GlucoseReading> {
+private fun getGlucoseReadings(readings: MutableState<List<GlucoseReading>>) {
 
-    var readings = emptyList<GlucoseReading>()
+    CoroutineScope(Dispatchers.IO).launch {
+
+        var foundReadings =
+                MainActivity.glucoseReadingDao.getAllFromUserByUserIndex(
+                        PreferenceManager.instance.getActiveUserIndex().toInt())
+        if (foundReadings.size == 1) {
+
+            foundReadings = listOf(GlucoseReading(0, 0f, Date()), foundReadings[0])
+        }
+        withContext(Dispatchers.Main) {
+
+            readings.value = foundReadings
+        }
+    }
+
+}
+
+fun generateData(readings: MutableState<List<GlucoseReading>>) {
+
     for (i in 1..30) {
         val randomGlucoseLevel = Random.nextFloat() * (25f - 3f) + 3f
         val time = Date(System.currentTimeMillis() + i * 1000L) // Incrementing timestamp for demo purposes
-        readings += GlucoseReading(0,  glucoseLevel = randomGlucoseLevel, time = time)
+        readings.value += GlucoseReading(0,  glucoseLevel = randomGlucoseLevel, time = time)
     }
 
-    return readings
 }
 
 @Preview

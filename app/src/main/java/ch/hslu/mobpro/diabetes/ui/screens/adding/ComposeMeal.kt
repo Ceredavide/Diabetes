616ed5505654b2c28ed934df2a38f02957461e2a
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2024-05-21 02:37:52 +0200

    Implemented database table for glucose readings

--- app/src/main/java/ch/hslu/mobpro/diabetes/ui/screens/adding/ComposeMeal.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/ui/screens/adding/ComposeMeal.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,16 +30,26 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import ch.hslu.mobpro.diabetes.MainActivity
+import ch.hslu.mobpro.diabetes.R
+import ch.hslu.mobpro.diabetes.data.database.GlucoseReading
+import ch.hslu.mobpro.diabetes.data.pref.PreferenceManager
 import ch.hslu.mobpro.diabetes.ui.components.IngredientListItem
 import ch.hslu.mobpro.diabetes.math.Ingredient
+import ch.hslu.mobpro.diabetes.ui.components.FloatTextField
 import ch.hslu.mobpro.diabetes.ui.navigation.Routes
 import ch.hslu.mobpro.diabetes.ui.viewmodels.IngredientViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.Date
 
 @Composable
 fun ComposeMeal(navController: NavController, ingredientViewModel: IngredientViewModel) {
 
     val ingredients = ingredientViewModel.ingredients
     var check by remember { mutableStateOf(false) }
+    var glucoseLevel by remember { mutableStateOf("") }
     val context = LocalContext.current
 
     Column {
@@ -52,20 +63,35 @@ fun ComposeMeal(navController: NavController, ingredientViewModel: IngredientVie
 
             Button(
                 onClick = {
-                    if (validateIngredients(ingredients)) {
+                    if (validateIngredients(ingredients) && validateGlucoseLevel(glucoseLevel.toFloatOrNull())) {
+
+                        CoroutineScope(Dispatchers.IO).launch {
+
+                            val currentTime = Date()
+                            val glucoseReading = GlucoseReading(
+                                userIndex = PreferenceManager.instance.getActiveUserIndex().toInt(),
+                                glucoseLevel = glucoseLevel.toFloat(),
+                                time = currentTime
+                            )
+
+                            MainActivity.glucoseReadingDao.insertGlucoseReading(glucoseReading)
+                        }
 
                         navController.navigate(Routes.resultScreen)
-                    }
-                    else if (ingredients.isEmpty()) {
+                    } else if (ingredients.isEmpty()) {
 
-                        Toast.makeText(context, "PLEASE ADD PRODUCTS TO THIS MEAL", Toast.LENGTH_LONG).show()
-                    }
-                    else {
+                        Toast.makeText(
+                            context,
+                            "PLEASE ADD PRODUCTS TO THIS MEAL",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else {
 
-                        Toast.makeText(context, "PLEASE ENTER MISSING VALUES", Toast.LENGTH_LONG).show()
+                        Toast.makeText(context, "PLEASE ENTER MISSING VALUES", Toast.LENGTH_LONG)
+                            .show()
                         check = true
                     }
-                          },
+                },
                 modifier = Modifier.padding(top = 10.dp)
             ) {
                 Text(text = "CALCULATE", color = Color.White)
@@ -88,16 +114,25 @@ fun ComposeMeal(navController: NavController, ingredientViewModel: IngredientVie
 
         Spacer(modifier = Modifier.padding(top = 16.dp))
 
+        FloatTextField(
+            value = glucoseLevel,
+            onValueChange = { glucoseLevel = it },
+            label = stringResource(id = R.string.glucose_level)
+        )
+
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+
         LazyColumn {
             items(ingredients.size) { index ->
                 Box(
                     modifier = Modifier.fillMaxWidth()
                 ) {
 
-                    IngredientListItem(ingredient = ingredients[index],
+                    IngredientListItem(
+                        ingredient = ingredients[index],
                         ingredientViewModel = ingredientViewModel,
                         check = check
-                        )
+                    )
                     Spacer(modifier = Modifier.padding(top = 60.dp))
                 }
             }
@@ -105,7 +140,12 @@ fun ComposeMeal(navController: NavController, ingredientViewModel: IngredientVie
     }
 }
 
-fun validateIngredients(ingredientsState: SnapshotStateList<Ingredient>): Boolean {
+private fun validateGlucoseLevel(glucoseLevel: Float?): Boolean {
+
+    return glucoseLevel != null && glucoseLevel > 0.0f
+}
+
+private fun validateIngredients(ingredientsState: SnapshotStateList<Ingredient>): Boolean {
 
     if (ingredientsState.size == 0) {
 

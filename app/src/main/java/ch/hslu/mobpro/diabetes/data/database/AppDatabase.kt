f893d87251d74015cc3fdb947f769ce4a76be69f
Davide Ceresa <ceredavide@live.it>
2024-05-22 22:04:38 +0200

    refactor: update RoomDatabase annotation syntax

--- app/src/main/java/ch/hslu/mobpro/diabetes/data/database/AppDatabase.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/data/database/AppDatabase.kt
@@ -3,7 +3,7 @@ package ch.hslu.mobpro.diabetes.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(Product::class), version = 1)
+@Database(entities = [Product::class], version = 1)
 abstract class AppDatabase: RoomDatabase() {
 
     companion object {

34061e85e7678150a56f11798df024cf344b40b9
Davide Ceresa <ceredavide@live.it>
2024-05-25 21:28:42 +0200

    refactor: moved SearchLocal.kt to the product.list package

--- app/src/main/java/ch/hslu/mobpro/diabetes/presentation/ui/product/list/SearchLocal.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/presentation/ui/product/list/SearchLocal.kt
@@ -1,4 +1,4 @@
-package ch.hslu.mobpro.diabetes.presentation.ui
+package ch.hslu.mobpro.diabetes.presentation.ui.product.list
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column

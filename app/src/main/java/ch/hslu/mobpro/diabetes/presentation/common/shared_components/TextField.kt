8075d79e773869060e67ae2c2f369ee04c587dd9
Davide Ceresa <ceredavide@live.it>
2024-05-25 19:02:59 +0200

    refactor: Create TextField component for shared input fields

---
+++ app/src/main/java/ch/hslu/mobpro/diabetes/presentation/common/shared_components/TextField.kt
@@ -0,0 +1,39 @@
+package ch.hslu.mobpro.diabetes.presentation.common.shared_components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+
+
+@Composable
+fun TextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Done,
+    error: String? = null
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Column {
+        OutlinedTextField(
+            value = if (value == "0.0") "" else value,
+            onValueChange = onValueChange,
+            label = { Text(label) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = keyboardType).copy(imeAction = imeAction),
+            keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() }),
+            isError = error != null
+        )
+        if (error != null) {
+            Text(error)
+        }
+    }
+}

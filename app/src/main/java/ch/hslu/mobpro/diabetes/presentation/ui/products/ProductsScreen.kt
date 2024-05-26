5a8b6d90cba1139d57c20bafaa05faea9d81657d
Davide Ceresa <ceredavide@live.it>
2024-05-26 15:22:55 +0200

    refactor: Update import statements and refactor code in ProductsScreen.kt

--- app/src/main/java/ch/hslu/mobpro/diabetes/presentation/ui/products/ProductsScreen.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/presentation/ui/products/ProductsScreen.kt
@@ -2,6 +2,7 @@ package ch.hslu.mobpro.diabetes.presentation.ui.products
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -9,7 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,13 +19,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -52,6 +56,7 @@ fun ProductsScreen(
     onTextInputChange(text.text, productsState)
 
     val productFormDialogViewModel = ProductFormDialogViewModel()
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     Column(
         modifier = Modifier
@@ -60,23 +65,32 @@ fun ProductsScreen(
     ) {
         ProductFormDialog(viewModel = productFormDialogViewModel)
 
-        Button(onClick = { productFormDialogViewModel.addProduct() }) {
-            Text(text = "Add Product")
-        }
 
-        val keyboardController = LocalSoftwareKeyboardController.current
-        OutlinedTextField(
-            value = text,
-            onValueChange = {
-                text = it
-                onTextInputChange(text.text, productsState)
-            },
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-            keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() }),
-            label = { Text(stringResource(id = R.string.product_name)) },
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
-        )
+                .padding(horizontal = 16.dp)
+        ) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 8.dp),
+                value = text,
+                onValueChange = {
+                    text = it
+                    onTextInputChange(text.text, productsState)
+                },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = { keyboardController?.hide() }),
+                label = { Text(stringResource(id = R.string.product_name)) }
+            )
+            FloatingActionButton(
+                onClick = { productFormDialogViewModel.addProduct() },
+                modifier = Modifier.align(Alignment.CenterVertically)
+            ) {
+                Text(text = "+", style = TextStyle(fontSize = 24.sp))
+            }
+        }
 
         Spacer(modifier = Modifier.height(16.dp))
 

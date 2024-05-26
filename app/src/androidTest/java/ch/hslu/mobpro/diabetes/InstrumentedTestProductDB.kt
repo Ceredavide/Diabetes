a1a699e90ac69b9527ea21647f83763392ab0436
Davide Ceresa <ceredavide@live.it>
2024-05-26 08:07:08 +0200

    refactor: Update import statements for Product and ProductDAO in InstrumentedTestProductDB.kt

--- app/src/androidTest/java/ch/hslu/mobpro/diabetes/InstrumentedTestProductDB.kt
+++ app/src/androidTest/java/ch/hslu/mobpro/diabetes/InstrumentedTestProductDB.kt
@@ -5,8 +5,8 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import ch.hslu.mobpro.diabetes.data.database.AppDatabase
-import ch.hslu.mobpro.diabetes.data.database.Product
-import ch.hslu.mobpro.diabetes.data.database.ProductDAO
+import ch.hslu.mobpro.diabetes.data.database.entity.Product
+import ch.hslu.mobpro.diabetes.data.database.dao.ProductDAO
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test

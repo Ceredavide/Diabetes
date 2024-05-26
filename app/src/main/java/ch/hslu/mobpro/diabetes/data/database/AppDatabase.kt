fd38c4d2c35add66204309d343174888b44ed6ef
Davide Ceresa <ceredavide@live.it>
2024-05-26 08:05:59 +0200

    refactor: Update import statements for GlucoseReading and GlucoseReadingDAO in InstrumentedTestGlucoseReadingsDB.kt

--- app/src/main/java/ch/hslu/mobpro/diabetes/data/database/AppDatabase.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/data/database/AppDatabase.kt
@@ -3,6 +3,10 @@ package ch.hslu.mobpro.diabetes.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import ch.hslu.mobpro.diabetes.data.database.dao.GlucoseReadingDAO
+import ch.hslu.mobpro.diabetes.data.database.dao.ProductDAO
+import ch.hslu.mobpro.diabetes.data.database.entity.GlucoseReading
+import ch.hslu.mobpro.diabetes.data.database.entity.Product
 
 @Database(entities = [Product::class, GlucoseReading::class], version = 1)
 @TypeConverters(Converters::class)

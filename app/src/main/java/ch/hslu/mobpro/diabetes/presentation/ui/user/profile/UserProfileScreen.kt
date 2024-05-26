13ad073b01117cf127614861c3153bf8769b6ee1
Davide Ceresa <ceredavide@live.it>
2024-05-26 15:50:50 +0200

    feat: add CurrentUserInfo to display user informations

--- app/src/main/java/ch/hslu/mobpro/diabetes/presentation/ui/user/profile/UserProfileScreen.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/presentation/ui/user/profile/UserProfileScreen.kt
@@ -2,6 +2,8 @@ package ch.hslu.mobpro.diabetes.presentation.ui.user.profile
 
 import android.content.Context
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.FloatingActionButton
@@ -17,11 +19,13 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import ch.hslu.mobpro.diabetes.MainActivity
 import ch.hslu.mobpro.diabetes.data.pref.PreferenceManager
 import ch.hslu.mobpro.diabetes.presentation.common.shared_components.ActiveUserIndicator
 import ch.hslu.mobpro.diabetes.presentation.ui.user.profile.components.UserListItem
 import ch.hslu.mobpro.diabetes.presentation.navigation.Routes
 import ch.hslu.mobpro.diabetes.presentation.common.shared_viewmodels.GlucoseReadingsViewModel
+import ch.hslu.mobpro.diabetes.presentation.ui.user.profile.components.CurrentUserInfo
 
 @Composable
 fun ProfileScreen(
@@ -33,34 +37,50 @@ fun ProfileScreen(
     val users = remember { PreferenceManager.instance.getAllUserInfo(context) }
 
     Column(
-            modifier = Modifier.padding(16.dp)
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
     ) {
 
-        ActiveUserIndicator(navController = navController)
+        val user = MainActivity.activeUserInfo.value!!
 
+        CurrentUserInfo(user)
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = androidx.compose.foundation.layout.Arrangement.SpaceBetween
+        )
+        {
+            Text(
+                text = "Registered Users: ${users.size}",
+                style = TextStyle(fontSize = 24.sp),
+                modifier = Modifier.padding(16.dp)
+            )
+            FloatingActionButton(
+                onClick = { navController.navigate(Routes.userFormCreate) }
+            ) {
+                Text(text = "+", style = TextStyle(fontSize = 48.sp))
+
+            }
+        }
         LazyColumn(
-                modifier = Modifier
-                    .padding(16.dp)
+            modifier = Modifier
+                .padding(16.dp)
         ) {
 
             items(users.size) { index ->
 
                 UserListItem(
-                        navController = navController,
-                        glucoseReadingsViewModel = glucoseReadingsViewModel,
-                        userName = users[index].name.value,
-                        userIndex = index.toUInt(),
-                        deletable = users.size > 1,
-                        context
+                    navController = navController,
+                    glucoseReadingsViewModel = glucoseReadingsViewModel,
+                    userName = users[index].name.value,
+                    userIndex = index.toUInt(),
+                    deletable = users.size > 1,
+                    context
                 )
             }
-        }
-
-        FloatingActionButton(
-                onClick = { navController.navigate(Routes.userFormCreate) }
-        ) {
-            Text(text = "+", style = TextStyle(fontSize = 48.sp))
-
         }
     }
 }
@@ -71,5 +91,5 @@ fun ProfileScreenPreview() {
 
     val navController = rememberNavController()
     val glucoseReadingsViewModel: GlucoseReadingsViewModel = viewModel()
-    ProfileScreen(navController, glucoseReadingsViewModel,LocalContext.current)
+    ProfileScreen(navController, glucoseReadingsViewModel, LocalContext.current)
 }

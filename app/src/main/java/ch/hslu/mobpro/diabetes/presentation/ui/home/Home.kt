11b804650e861ad6d8bf92d9a69671bf81d3f234
Davide Ceresa <ceredavide@live.it>
2024-05-26 08:07:39 +0200

    refactor: Remove unused code and update HomeScreen UI in Home.kt

--- app/src/main/java/ch/hslu/mobpro/diabetes/presentation/ui/home/Home.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/presentation/ui/home/Home.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,105 +17,42 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import ch.hslu.mobpro.diabetes.data.database.GlucoseReading
 import ch.hslu.mobpro.diabetes.presentation.common.ActiveUserIndicator
 import ch.hslu.mobpro.diabetes.presentation.common.FloatTextField
 import ch.hslu.mobpro.diabetes.presentation.ui.home.components.Graph
-import ch.hslu.mobpro.diabetes.presentation.ui.home.components.persistGlucoseReading
 import ch.hslu.mobpro.diabetes.presentation.common.shared_viewmodels.GlucoseReadingsViewModel
-import java.util.Date
-import kotlin.random.Random
+import ch.hslu.mobpro.diabetes.presentation.navigation.Routes
 
 @Composable
 fun HomeScreen(navController: NavController, glucoseReadingsViewModel: GlucoseReadingsViewModel) {
 
-    var readingInput by remember { mutableStateOf("") }
-
     Column(
-            verticalArrangement = Arrangement.Top
+        verticalArrangement = Arrangement.Top
     ) {
 
         ActiveUserIndicator(navController = navController)
 
         Graph(
-                glucoseReadingsViewModel = glucoseReadingsViewModel,
-                height = 500.dp
+            glucoseReadingsViewModel = glucoseReadingsViewModel,
+            height = 500.dp
         )
 
         Column(
-                modifier = Modifier
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
-            FloatTextField(
-                    value = readingInput,
-                    onValueChange = { readingInput = it },
-                    label = "Glucose reading(mmol/L)"
-            )
-
             Button(
-                    onClick = {
-                        val reading = readingInput.toFloatOrNull()
-                        if (reading != null && reading > 0.0f) {
-
-                            persistGlucoseReading(glucoseLevel =  reading, glucoseReadingsViewModel = glucoseReadingsViewModel)
-                            readingInput = ""
-                        }
-                    }
-            ) {
-
-                Text(text = "ADD GLUCOSE READING")
-            }
-        }
-
-        /*
-        Button(
-                modifier = Modifier,
                 onClick = {
+                    navController.navigate(Routes.composeMeal)
                 }
-        ) {
-            Text("Generate Data")
-        }
-
-         */
-    }
-}
-
-/*
-private fun getGlucoseReadings(readings: List<GlucoseReading>) {
-
-    CoroutineScope(Dispatchers.IO).launch {
-
-        var foundReadings =
-                MainActivity.glucoseReadingDao.getAllFromUserByUserIndex(
-                        PreferenceManager.instance.getActiveUserIndex().toInt()
-                )
-        if (foundReadings.size == 1) {
-
-            foundReadings = listOf(GlucoseReading(0, 0f, Date()), foundReadings[0])
-        }
-        else {
-            withContext(Dispatchers.Main) {
-
-                readings = foundReadings
+            ) {
+                Text(text = "Add Meal")
             }
         }
     }
-
 }
- */
 
-fun generateData(readings: MutableState<List<GlucoseReading>>) {
-
-    for (i in 1..30) {
-        val randomGlucoseLevel = Random.nextFloat() * (25f - 3f) + 3f
-        val time =
-                Date(System.currentTimeMillis() + i * 1000L) // Incrementing timestamp for demo purposes
-        readings.value += GlucoseReading(0, glucoseLevel = randomGlucoseLevel, time = time)
-    }
-
-}
 
 @Preview
 @Composable

cfb5d4bb96a39c6e1cac6f242c328a1676b6fcbc
Davide Ceresa <ceredavide@live.it>
2024-05-26 15:36:36 +0200

    refactor: Move ActiveUserIndicator import to shared_components package

--- app/src/main/java/ch/hslu/mobpro/diabetes/presentation/ui/home/Home.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/presentation/ui/home/Home.kt
@@ -6,10 +6,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -17,8 +13,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import ch.hslu.mobpro.diabetes.presentation.common.ActiveUserIndicator
-import ch.hslu.mobpro.diabetes.presentation.common.FloatTextField
+import ch.hslu.mobpro.diabetes.presentation.common.shared_components.ActiveUserIndicator
 import ch.hslu.mobpro.diabetes.presentation.ui.home.components.Graph
 import ch.hslu.mobpro.diabetes.presentation.common.shared_viewmodels.GlucoseReadingsViewModel
 import ch.hslu.mobpro.diabetes.presentation.navigation.Routes

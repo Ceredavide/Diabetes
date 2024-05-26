5dc9b2e499b1dd13249426438bdeb8f6ee4fd3fc
Davide Ceresa <ceredavide@live.it>
2024-05-26 08:08:36 +0200

    refactor: Update import statements and refactor code in multiple files

--- app/src/main/java/ch/hslu/mobpro/diabetes/presentation/ui/app/AppScreen.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/presentation/ui/app/AppScreen.kt
@@ -4,86 +4,127 @@ import android.annotation.SuppressLint
 import android.content.Context
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import ch.hslu.mobpro.diabetes.data.pref.PreferenceManager
 import ch.hslu.mobpro.diabetes.presentation.common.shared_viewmodels.GlucoseReadingsViewModel
 import ch.hslu.mobpro.diabetes.presentation.common.shared_viewmodels.IngredientViewModel
 import ch.hslu.mobpro.diabetes.presentation.navigation.BottomNavigationBar
 import ch.hslu.mobpro.diabetes.presentation.navigation.Routes
-import ch.hslu.mobpro.diabetes.presentation.ui.user.profile.ProfileScreen
-import ch.hslu.mobpro.diabetes.presentation.ui.ResultScreen
-import ch.hslu.mobpro.diabetes.presentation.ui.adding.AddUser
-import ch.hslu.mobpro.diabetes.presentation.ui.adding.ComposeMeal
-import ch.hslu.mobpro.diabetes.presentation.ui.editing.EditUser
 import ch.hslu.mobpro.diabetes.presentation.ui.home.HomeScreen
-import ch.hslu.mobpro.diabetes.presentation.ui.product.form.ProductFormScreen
-import ch.hslu.mobpro.diabetes.presentation.ui.product.list.SearchLocalScreen
-
+import ch.hslu.mobpro.diabetes.presentation.ui.home.components.ResultScreen
+import ch.hslu.mobpro.diabetes.presentation.ui.home.components.composeMeal.ComposeMeal
+import ch.hslu.mobpro.diabetes.presentation.ui.products.ProductsScreen
+import ch.hslu.mobpro.diabetes.presentation.ui.user.profile.ProfileScreen
+import ch.hslu.mobpro.diabetes.presentation.ui.user.form.UserFormScreen
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun AppScreen(context: Context) {
     val navController = rememberNavController()
-    val ingredientViewModel: IngredientViewModel = viewModel()
-    val glucoseReadingsViewModel: GlucoseReadingsViewModel = viewModel()
-    val glucoseLevel = remember { mutableFloatStateOf(0.0f) }
+
+    val viewModelStoreOwner = LocalViewModelStoreOwner.current
+
+    val ingredientViewModel: IngredientViewModel = viewModel(
+        viewModelStoreOwner = viewModelStoreOwner!!,
+        key = "ingredientViewModel",
+        factory = ViewModelProvider.NewInstanceFactory()
+    )
+
+    val glucoseReadingsViewModel: GlucoseReadingsViewModel = viewModel(
+        viewModelStoreOwner = viewModelStoreOwner,
+        key = "glucoseReadingsViewModel",
+        factory = ViewModelProvider.NewInstanceFactory()
+    )
 
     Scaffold(bottomBar = { BottomNavigationBar(navController) }) {
         NavHost(navController, startDestination = Routes.home) {
-            composable(Routes.home) {
-                HomeScreen(navController = navController, glucoseReadingsViewModel = glucoseReadingsViewModel)
-            }
-            composable(Routes.enterManually) {
-                ProductFormScreen()
-            }
-            composable(Routes.editProduct + "/{name}/{carbs}") { backStackEntry ->
-                val productName = backStackEntry.arguments?.getString("name")
-                val productCarbs = backStackEntry.arguments?.getString("carbs")?.toFloat()
-                if (productName != null && productCarbs != null) {
-                    ProductFormScreen(initialProductName = productName, initialCarbs = productCarbs)
-                }
-            }
-            composable(Routes.searchLocal + "/{editable}") { backStackEntry ->
-                val editable = backStackEntry.arguments?.getString("editable")?.toBoolean() ?: false
-                SearchLocalScreen(navController = navController, editable = editable, ingredientViewModel = ingredientViewModel)
-            }
-            composable(Routes.searchLocal) {
-                SearchLocalScreen(navController = navController, editable = true, ingredientViewModel = ingredientViewModel)
-            }
-            composable(Routes.composeMeal) {
-                ComposeMeal(
+            homeGraph(navController, glucoseReadingsViewModel, ingredientViewModel, context)
+            userGraph(navController, glucoseReadingsViewModel, context)
+
+            composable(Routes.products) {
+                ProductsScreen(
                     navController = navController,
-                    ingredientViewModel = ingredientViewModel,
-                    glucoseReadingsViewModel = glucoseReadingsViewModel,
-                    glucoseLevel = glucoseLevel
+                    editable = true,
+                    ingredientViewModel = null
                 )
             }
-            composable(Routes.resultScreen) {
-                ResultScreen(
-                    navController = navController,
-                    ingredientViewModel = ingredientViewModel,
-                    glucoseLevel = glucoseLevel.floatValue,
-                    context = context
-                )
-            }
-            composable(Routes.notifications) {
-                ProfileScreen(navController = navController, glucoseReadingsViewModel = glucoseReadingsViewModel, context = context)
-            }
-            composable(Routes.editUser + "/{username}") { backStackEntry ->
-                val userName = backStackEntry.arguments?.getString("username")
-                val userInfo = userName?.let { PreferenceManager.instance.getUserInfo(it, context) }
-                if (userInfo != null) {
-                    EditUser(navController = navController, user = userInfo, context = context)
-                }
-            }
-            composable(Routes.addUser) {
-                AddUser(navController = navController, context = context)
-            }
+        }
+    }
+}
+
+fun NavGraphBuilder.homeGraph(
+    navController: NavController,
+    glucoseReadingsViewModel: GlucoseReadingsViewModel,
+    ingredientViewModel: IngredientViewModel,
+    context: Context
+) {
+    navigation(startDestination = Routes.dashboard, route = Routes.home) {
+        composable(Routes.dashboard) {
+            HomeScreen(
+                navController = navController,
+                glucoseReadingsViewModel = glucoseReadingsViewModel
+            )
+        }
+        composable(Routes.composeMeal) {
+            ComposeMeal(
+                navController = navController,
+                ingredientViewModel = ingredientViewModel,
+                glucoseReadingsViewModel = glucoseReadingsViewModel
+            )
+        }
+        composable(Routes.uneditableProducts) {
+            ProductsScreen(
+                navController = navController,
+                editable = false,
+                ingredientViewModel = ingredientViewModel
+            )
+        }
+        composable(Routes.resultScreen) { backStackEntry ->
+            val glucoseLevel = backStackEntry.arguments?.getFloat("glucoseLevel")
+            ResultScreen(
+                navController = navController,
+                context = context,
+                ingredientViewModel = ingredientViewModel,
+                glucoseLevel = glucoseLevel!!
+            )
+        }
+    }
+}
+
+fun NavGraphBuilder.userGraph(
+    navController: NavController,
+    glucoseReadingsViewModel: GlucoseReadingsViewModel,
+    context: Context
+) {
+    navigation(startDestination = Routes.profile, route = Routes.user) {
+        composable(Routes.profile) {
+            ProfileScreen(
+                navController = navController,
+                glucoseReadingsViewModel = glucoseReadingsViewModel,
+                context = context
+            )
+        }
+        composable(Routes.userFormCreate) { _ ->
+            UserFormScreen(
+                navController = navController,
+                user = null,
+            )
+        }
+        composable(Routes.userFormEdit) { backStackEntry ->
+            val userName = backStackEntry.arguments?.getString("userName")
+            val userInfo = userName?.let { PreferenceManager.instance.getUserInfo(it, context) }
+            UserFormScreen(
+                navController = navController,
+                user = userInfo,
+            )
         }
     }
 }

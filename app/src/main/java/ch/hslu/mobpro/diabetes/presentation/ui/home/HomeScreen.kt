8e203f4131bc18466ef2dcff068466edd0e0aa42
Davide Ceresa <ceredavide@live.it>
2024-05-26 18:43:03 +0200

    refactor: Update HomeScreen UI, add GraphCard component and remove ActiveUserIndicator

--- app/src/main/java/ch/hslu/mobpro/diabetes/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/presentation/ui/home/HomeScreen.kt
@@ -2,17 +2,16 @@ package ch.hslu.mobpro.diabetes.presentation.ui.home
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import ch.hslu.mobpro.diabetes.presentation.common.shared_components.ActiveUserIndicator
 import ch.hslu.mobpro.diabetes.presentation.common.shared_components.Header
 import ch.hslu.mobpro.diabetes.presentation.ui.home.components.Graph
 import ch.hslu.mobpro.diabetes.presentation.common.shared_viewmodels.GlucoseReadingsViewModel
@@ -43,18 +42,41 @@ fun HomeContent(
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        ActiveUserIndicator(navController = navController)
-
         Spacer(modifier = Modifier.height(16.dp))
 
-        Graph(
-            glucoseReadingsViewModel = glucoseReadingsViewModel,
-            height = 300.dp
-        )
+        GraphCard(glucoseReadingsViewModel)
 
         Spacer(modifier = Modifier.height(16.dp))
 
         AddMealButton(navController)
+    }
+}
+
+@Composable
+fun GraphCard(glucoseReadingsViewModel: GlucoseReadingsViewModel) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 4.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 4.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Glucose Level Over Time:",
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Graph(
+                glucoseReadingsViewModel = glucoseReadingsViewModel,
+                height = 300.dp
+            )
+        }
     }
 }
 
@@ -64,7 +86,7 @@ fun AddMealButton(navController: NavController) {
         onClick = { navController.navigate(Routes.composeMeal) },
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 32.dp)
+            .padding(horizontal = 4.dp)
     ) {
         Text(text = "Add Meal")
     }

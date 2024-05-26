4475de32b4addaa5a3af4319156da137028c2c84
Davide Ceresa <ceredavide@live.it>
2024-05-26 08:06:17 +0200

    refactor: Rename ProductDAO.kt to ProductDao.kt and move it to the database.dao package

--- app/src/main/java/ch/hslu/mobpro/diabetes/data/database/dao/ProductDAO.kt
+++ app/src/main/java/ch/hslu/mobpro/diabetes/data/database/dao/ProductDAO.kt
@@ -1,4 +1,4 @@
-package ch.hslu.mobpro.diabetes.data.database
+package ch.hslu.mobpro.diabetes.data.database.dao
 
 import androidx.room.Dao
 import androidx.room.Delete
@@ -6,6 +6,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
+import ch.hslu.mobpro.diabetes.data.database.entity.Product
 
 @Dao
 interface ProductDAO {
